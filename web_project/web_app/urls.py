8155bda1b015e748442d9f4158a5a847f74ee275
mary lazareva <lazareva.m1989@yandex.ru>
2024-02-12 12:48:51 +0300

    homework_3

--- web_project/web_app/urls.py
+++ web_project/web_app/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
-    path('info/<int:order_id>/', views.info, name='info')
+    path('info/<int:order_id>/', views.info, name='info'),
+    path('customer/<int:customer_id>', views.show_customers_orders, name='show_customers_orders'),
+    path('last_orders/<int:customer_id>', views.show_recent_orders, name='show_recent_orders'),
 
 ]
--- web_project/web_app/views.py
+++ web_project/web_app/views.py
@@ -1,8 +1,10 @@
+from datetime import timedelta
+from django.utils import timezone
 from django.http import HttpResponse
 from django.shortcuts import render
 import logging
 
-from web_app.models import Order
+from web_app.models import Order, Customer, OrderItem
 
 logger = logging.getLogger(__name__)
 
@@ -37,3 +39,50 @@ def info(request, order_id):
     return HttpResponse(f'{order.customer.name} has Order{order.id}, total_sum is {total_price}:<br>.'
                         f'{products_list}<br>'
                         f'Registrated at {order.customer.date_registrated} ')
+
+
+def show_customers_orders(request, customer_id):
+    customer = Customer.objects.get(pk=customer_id)
+    orders = Order.objects.filter(customer=customer)
+
+    orders_products = []
+    for order in orders:
+        # Извлекаем все данные о конкретном заказе
+        order_items = OrderItem.objects.filter(order=order)
+
+        # Товары с данными о количестве
+        products_with_quantities = []
+        for item in order_items:
+            product = item.product
+            quantity = item.quantity
+
+            products_with_quantities.append((product, quantity))
+
+        orders_products.append((order, products_with_quantities))
+
+    context = {'orders_products': orders_products, 'customer': customer}
+    return render(request, "web_app/customer.html", context)
+
+
+def show_recent_orders(request, customer_id):
+    now = timezone.now()  # получение текущего времени
+    periods = {
+        'week': now - timedelta(days=7),  # определение временных промежутков: за последнюю неделю
+        'month': now - timedelta(days=30),
+        'year': now - timedelta(days=365),
+    }
+
+    orders_data = {}
+    customer = Customer.objects.get(pk=customer_id)
+
+    for period, start_date in periods.items():
+        orders_data[period] = OrderItem.objects.filter(
+            order__customer=customer,      #  выбираем OrderItem объекты, где связанный Order принадлежит данному Customer
+            order__date_ordered__gte=start_date   #  фильтрует OrderItem объекты, выбирая только те, дата создания заказа (date_ordered) которых больше или равна заданной начальной дате (start_date).
+        ).select_related('order', 'product').order_by('-order__date_ordered')  # сортирует результаты по убыванию даты заказа, так что самые новые заказы будут первыми
+
+    context = {
+        'orders_data': orders_data,
+        'customer': customer
+    }
+    return render(request, "web_app/recent_orders.html", context)
